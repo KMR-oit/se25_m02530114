187dc3da42fabad28196ea36944d3e58a9bfe4d5
KMR_m1m02530114 <m1m02530114@oit.ac.jp>
2025-06-23 00:35:40 +0900

    Main.java Player.java

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
-    
+    Player player = new Player();
+
+    player.drawMonsters();
+    player.showDeck();
+
+    // Monster monster = new Monster();
+    // System.out.println(monster.toString());
+
   }
 }
